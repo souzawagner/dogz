0b99b657c40794ceaa4110e3db9cf4c45f05d23a
Wagner Souza <wagner.souza@accenture.com>
2022-03-12 16:39:14 -0300

    Updates

--- _next/static/2Gla1h1GsLC5M8C91-bI9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ea18d4408f2790bb.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
